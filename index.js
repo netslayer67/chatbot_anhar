ab3596bb102e8fb6edc1ca36aa98fc642a052ab2
Sayed Jilliyan <netslayer71@gmail.com>
2024-05-29 18:47:17 +0700

    test

--- index.js
+++ index.js
@@ -203,7 +203,7 @@ async function connectionLogic() {
                                 'Jenis Hp : \n' +
                                 'RAM HP (khusus Android) :\n' +
                                 'Simcard : (masih ada/ sudah tidak ada)\n' +
-                                'Rekomendasi : Handoko\n\n' +
+                                'Rekomendasi : *Syahrial Anhar*\n\n' +
                                 '𝗔𝗣𝗟𝗜𝗞𝗔𝗦𝗜 𝗬𝗔𝗡𝗚 𝗠𝗔𝗦𝗜𝗛 𝗕𝗘𝗥𝗝𝗔𝗟𝗔𝗡 / 𝗗𝗜𝗥𝗔𝗪𝗔𝗧 :\n' +
                                 '1. Nama aplikasi : \n' +
                                 '     Total Limit : \n' +
